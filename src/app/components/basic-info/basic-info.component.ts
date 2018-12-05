f93249847624e65b6d0d0e3c5aff7258dd0b6c21
Lillian Lee <lillianlee62@gmail.com>
2018-12-04 21:55:04 -0500

    zip

--- src/app/components/basic-info/basic-info.component.ts
+++ src/app/components/basic-info/basic-info.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicInfoComponent  {
 
-  person = new Person('Millie','MillieLee@gmail.com', '1234567890','September 14, 2015');
+  person = new Person('Millie','123 Market Street','apt 123','Baltimore','MD','22111','MillieLee@gmail.com', '1234567890','September 14, 2015');
   title = "bioinfo";
   submitted = false;
 
@@ -17,6 +17,10 @@ export class BasicInfoComponent  {
   submit() {
     console.log(this.person);
   }  
+
+ // onZipChange() {
+ //   console.log(this.person.zip);
+  //}
   
   // TODO: remove this when we're done
  // get diagnostic() { return JSON.stringify(this.model);}
@@ -27,11 +31,21 @@ export class BasicInfoComponent  {
   
 class Person {
   name: String;
+  street1: string;
+  street2: string;
+  city: string;
+  state: string;
+  zip: string;
   email: String;
   ssn: String;
   dob: String;
-  constructor(name:String, email, ssn:String, dob:String) {
+  constructor(name:String, street1:string, street2:string, city:string, state:string, zip:string, email:string, ssn:String, dob:String) {
     this.name = name;
+    this.street1 = street1;
+    this.street2 = street2;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
     this.email = email;
     this.ssn = ssn;
     this.dob = dob;
